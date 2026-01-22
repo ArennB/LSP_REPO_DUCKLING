2ae70c70b1559006dba2f5fb5f3c943f3605ee30
ArennB <arenn.banks@bison.howard.edu>
2026-01-22 00:24:20 -0500

    Update HelloWorld.java

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,10 @@
+/**
+* Name: Arenn Banks
+*/
+
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-    /**
-     * Name: Arenn Banks
-     * @param args
-     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
